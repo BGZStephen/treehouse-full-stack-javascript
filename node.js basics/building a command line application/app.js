3d938aa88af5242b202abc3149e6e186d5634731
Stephen Wright <sjw948@gmail.com>
2017-02-28 19:29:17 +0000

    node.js get & JSON parse

--- node.js basics/building a command line application/app.js
+++ node.js basics/building a command line application/app.js
@@ -2,17 +2,34 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
 const https = require(`https`);
-const username = `chalkers`;
+// const username = `stephenwright2`;
+
 
 function printMessage(username, badgeCount, points) {
   const message = `${username} has ${badgeCount} badges and ${points} points in JavaScript.`;
   console.log(message);
 }
 
-// connect to the API url https://teamtreehouse.com/username.json
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  console.dir(response.statusCode)
-  // read the data
-  // parse the data
-  // print the data
-})
+function getProfile(username) {
+  // connect to the API url https://teamtreehouse.com/username.json
+  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    let body = ``;
+    console.dir(response.statusCode)
+    // read the data
+    response.on(`data`, data => {
+      body += data.toString();
+    });
+
+    response.on(`end`, () => {
+      const profile = JSON.parse(body)
+      printMessage(username, profile.badges.length, profile.points.JavaScript)
+    })
+    // parse the data
+    // print the data
+  })
+}
+const users = process.argv.slice(2)
+
+console.log(process.argv);
+
+users.forEach(getProfile)
