1fc8ebaf6e30e7cd30312d92bb416b898ef5be35
Stephen Wright <sjw948@gmail.com>
2017-03-05 15:32:07 +0000

    installed express and set up basic routes

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -1,11 +1,25 @@
 `use strict`;
 
 const express = require(`express`);
+const posts = require(`./mock/posts.json`);
 
 const app = express();
 
 app.get(`/`, function(req, res) {
-  res.send(`Hello World!`)
+  res.send(`<h1>Hello World!</h1>`)
 })
 
-app.listen(3000)
+app.get(`/blog/:title?`, function(req, res) {
+  const title = req.params.title
+  if(title === undefined) {
+    res.status(503);
+    res.send(`This page is under construction`)
+  } else {
+    const post = posts[title]
+    res.send(post)
+  }
+})
+
+app.listen(3000, function () {
+  console.log(`Frontend server running on port 3000`)
+})
