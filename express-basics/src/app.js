88a0cd40dc63b05d18812a9669d8a6bf50e110cf
Stephen Wright <sjw948@gmail.com>
2017-03-06 19:53:10 +0000

    final commit

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -3,6 +3,9 @@
 const express = require(`express`);
 const posts = require(`./mock/posts.json`);
 const pug = require(`pug`);
+const postsLists = Object.keys(posts).map(function(value) {
+  return posts[value]
+})
 
 const app = express();
 
@@ -12,18 +15,26 @@ app.set(`view engine`, `pug`)
 app.set(`views`, __dirname + `/templates`)
 
 app.get(`/`, function(req, res) {
+  const path = req.path;
+  res.locals.path = path;
   res.render(`index`)
 })
-
 
 app.get(`/blog/:title?`, function(req, res) {
   const title = req.params.title
   if(title === undefined) {
-    res.status(503);
-    res.send(`This page is under construction`)
+    res.render(`blog`, {posts: postsLists})
   } else {
     const post = posts[title] || {}
     res.render(`post`, {post: post})
+  }
+})
+
+app.get(`/posts`, function(req, res) {
+  if (req.query.raw) {
+    res.json(posts)
+  } else {
+    res.json(postsLists)
   }
 })
 
