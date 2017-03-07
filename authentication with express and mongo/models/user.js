c292a39a9e7180c12c69861eb0af4da990992065
Stephen Wright <sjw948@gmail.com>
2017-03-07 22:00:22 +0000

    added bcrypt for hashing and salting pre-mongodb submission

--- authentication with express and mongo/models/user.js
+++ authentication with express and mongo/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require(`mongoose`);
+var bcrypt = require(`bcrypt`)
 var UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -20,6 +21,17 @@ var UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
+});
+// has password before saving
+UserSchema.pre(`save`, function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 })
 
 var User = mongoose.model(`User`, UserSchema)
