fbd6ed2542385be459a754d258929dfba1559542
Stephen Wright <sjw948@gmail.com>
2017-03-12 08:48:37 +0000

    added before, beforeEach and teardowns

--- javascript unit testing/battleship-engine/test/ship_test.js
+++ javascript unit testing/battleship-engine/test/ship_test.js
@@ -2,43 +2,33 @@ var expect = require(`chai`).expect
 
 describe(`checkForShip`, function() {
   var checkForShip = require(`../game_logic/ship_methods`).checkForShip
+  var player;
+
+  player = {
+    ships: [
+      {
+        locations: [[0, 0], [0, 1]]
+      },
+      {
+        locations: [[1, 1], [6, 1]]
+      },
+      {
+        locations: [[4, 1], [5, 1], [3, 1], [0, 1]]
+      }
+    ]
+  };
 
   it(`should correctly report no ship at a given players coordinate`, function() {
-    player = {
-      ships: [
-        {
-          locations: [[0, 0]]
-        }
-      ]
-    };
     expect(checkForShip(player, [9, 9])).to.be.false
   }),
 
   it(`should correctly report a ship at a given players coordinate`, function() {
-    player = {
-      ships: [
-        {
-          locations: [[0, 0]]
-        }
-      ]
-    };
+
     expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
   }),
 
   it(`should handle ships located at more than 1 coordinate`, function() {
-    player = {
-      ships: [
-        {
-          locations: [[0, 0], [0, 1]]
-        },
-        {
-          locations: [[1, 1], [6, 1]]
-        },
-        {
-          locations: [[4, 1], [5, 1], [3, 1], [0, 1]]
-        }
-      ]
-    };
+
     expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
     expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
     expect(checkForShip(player, [9, 9])).to.be.false
@@ -48,7 +38,7 @@ describe(`checkForShip`, function() {
   })
 })
 
-describe(`checkForShip`, function() {
+describe(`damageShip`, function() {
   var damageShip = require(`../game_logic/ship_methods`).damageShip
 
   it(`should register damage on a given ship at a given location`, function() {
@@ -66,9 +56,10 @@ describe(`checkForShip`, function() {
 
 describe(`fire`, function() {
   var fire = require(`../game_logic/ship_methods`).fire
+  var player;
 
-  it(`should record damage on the given players ship at a given coordinate`, function() {
-    var player = {
+  beforeEach(function() {
+    player = {
       ships: [
         {
           locations: [[0, 0]],
@@ -76,6 +67,18 @@ describe(`fire`, function() {
         }
       ]
     }
+  })
+
+  after(function() {
+    console.log(`entire test suite completed`)
+  })
+
+  afterEach(function(){
+    console.log(`one unit test completed`)
+  })
+
+  it(`should record damage on the given players ship at a given coordinate`, function() {
+
     fire(player, [0, 0]);
     expect(player.ships[0].damage[0]).to.deep.equal[0, 0]
   }); // end it
